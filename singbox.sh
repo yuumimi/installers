47329aee9c4ff62a595910cc447ae5419865f802
yuumimi <110084151+yuumimi@users.noreply.github.com>
2022-11-30 10:27:58 +0800

    Add files via upload

--- singbox.sh
+++ singbox.sh
@@ -596,7 +596,7 @@ __bootstrap_webi() {
 				fi
 				;;
 			windows)
-				printf "\n以 \e[32mVPN / TUN / TAP / 增强模式\e[0m启动...\n"
+				printf "\n以 \e[32mVPN / TUN / TAP / 增强模式\e[0m启动...\n如果启动成功,此模式可在本窗口上按 Ctrl + C 或者关闭本窗口来退出 sing-box.\n\n"
 				set +e
 				pid=$(ps aux | grep "[s]ing-box" | awk '{print $1}')
 				_sudo "$pkg_dst_cmd" run -D "$WEBI_PKG_WORKDIR"
@@ -604,7 +604,7 @@ __bootstrap_webi() {
 					pid=$(ps aux | grep "[s]ing-box" | awk '{print $1}')
 					_sudo "$pkg_dst_cmd" run -D "$WEBI_PKG_WORKDIR"
 					if [ -z "${pid:-}" ]; then
-						printf "\n权限不足, VPN / TUN / TAP / 增强模式启动失败,以\e[32m系统代理模式\e[0m启动...\n"
+						printf "\n权限不足, VPN / TUN / TAP / 增强模式启动失败,以\e[32m系统代理模式\e[0m启动...\n此模式只能通过在本窗口上按  Ctrl + C 退出 sing-box,关闭本窗口 sing-box 会在后台继续运行\n"
 						cp -f "${WEBI_PKG_WORKDIR}/config.json" "${WEBI_PKG_WORKDIR}/config_system_proxy.json"
 						inbounds_tun=$(sed -n '/inbounds/=' "${WEBI_PKG_WORKDIR}/config_system_proxy.json")
 						sed -i "$((inbounds_tun + 1)),$((inbounds_tun + 11))d" "${WEBI_PKG_WORKDIR}/config_system_proxy.json"
