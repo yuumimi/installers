8a5d6001a4deb073c99e891ec0d88dacb50ee1ee
yuumimi <110084151+yuumimi@users.noreply.github.com>
2022-12-24 13:37:59 +0800

    Add files via upload

--- singbox.sh
+++ singbox.sh
@@ -371,7 +371,7 @@ __bootstrap_webi() {
 				if [[ $(sfc 2>&1 | tr -d '\0') =~ SCANNOW ]]; then
 					"${@}"
 				else
-					printf "\n权限不足,必须以管理员身份运行 Git Bash.\n右键点击 Git Bash 图标 > 属性 > 兼容性 > 勾选以管理员身份运行此程序 > 确定\n\n"
+					printf "\n权限不足,必须以管理员身份运行 Git Bash:\n右键点击桌面上的 Git Bash 图标 > 属性 > 兼容性 > 勾选以管理员身份运行此程序 > 确定\n\n"
 					"${@}"
 				fi
 				;;
